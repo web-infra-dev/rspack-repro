26f4a5892198ed6c3a3c4d7f2ef2a77386819743
hardfist <yangjianzju@gmail.com>
2024-08-15 12:21:10 +0800

    chore: failed to rename __nested_webpack_require

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-import('./render').then(exports => {
-    exports.render()
-})
+require('./other');
---
+++ src/other.js
@@ -0,0 +1,4 @@
+
+var __webpack_require__ = {};
+console.log(__webpack_require__);
+require('./answer');
